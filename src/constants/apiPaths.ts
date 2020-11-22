2af3bee4e6b7a2efe2de1ecac08666564c541f5c
Maksim_Mozhorov@epam.com <Maksim_Mozhorov@epam.com>
2020-11-22 23:02:33 +0300

    Updated paths for new urls

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 
 const API_PATHS = {
-  product: 'https://ifc1wmdluk.execute-api.us-east-1.amazonaws.com/dev',
+  product: 'https://y8lttn5dk4.execute-api.us-east-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://ifc1wmdluk.execute-api.us-east-1.amazonaws.com/dev'
+  import: 'https://h54kdtbmjl.execute-api.us-east-1.amazonaws.com/dev',
+  bff: 'https://y8lttn5dk4.execute-api.us-east-1.amazonaws.com/dev'
 };
 
 export default API_PATHS;
